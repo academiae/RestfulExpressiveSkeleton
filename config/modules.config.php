fe7d92367374ab2932b2bdccc822faaa7f6bcee1
gabbydgab <gamba@gabbydgab.com>
2016-08-16 10:10:49 +0800

    load zend-db and zend-hydrator components

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,9 +6,10 @@ use Zend\Expressive\ConfigManager\ConfigManager;
  * Use Fully Qualified Namespace to enable the expressive configuration
  */
 $modules = [
+    Zend\Db\ConfigProvider::class,
     CodingMatters\Rest\ConfigProvider::class,
     CodingMatters\Student\ConfigProvider::class,
-    CodingMatters\Employee\ConfigProvider::class,
+    CodingMatters\Employee\ConfigProvider::class,    
     //Append module namespace here
 ];
 
