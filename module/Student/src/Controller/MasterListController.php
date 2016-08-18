50bbd3cf0100d25f998efceaa03c607567747928
gabbydgab <gamba@gabbydgab.com>
2016-08-18 13:16:21 +0800

    updated implementation

--- module/Student/src/Controller/MasterListController.php
+++ module/Student/src/Controller/MasterListController.php
@@ -26,7 +26,8 @@
 
 namespace CodingMatters\Student\Controller;
 
-use CodingMatters\Student\Repository\MasterListRepository;
+//use CodingMatters\Student\Repository\MasterListRepository;
+use CodingMatters\Student\Repository\RestRepository;
 use CodingMatters\Rest\Controller\AbstractRestController;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -35,14 +36,14 @@ use CodingMatters\Student\Entity\StudentEntity;
 final class MasterListController extends AbstractRestController
 {
     /**
-     * @var MasterListRepository
+     * @var RestRepository
      */
     private $repository;
 
     /**
-     * @param MasterListRepository $repository
+     * @param RestRepository $repository
      */
-    public function __construct(MasterListRepository $repository)
+    public function __construct(RestRepository $repository)
     {
         $this->repository = $repository;
     }
@@ -55,21 +56,17 @@ final class MasterListController extends AbstractRestController
         $id = $request->getAttribute(self::IDENTIFIER_NAME);
         $student = $this->repository->fetchById($id);
 
-        if ($student->count() > 0) {
-            return $this->createResponse($student->toArray());
+        // Return error message if data is missing
+        if ($student->count() === 0) {
+            $code       = 404;
+            $status     = 'Not Found';
+            $message    = sprintf("Student with id '%s' not found.", $id);
+
+            return $this->createResponse($this->formatMessage($code, $status, $message), $code);
         }
 
-        // Throw error message if id is missing
-        $status         = 404;
-        $notFound       = 'Not Found';
-        $error_message  = sprintf("Student with id '%s' {$notFound}.", $id);
-        $data = [
-            'error'     => $this->responsePhraseToCode($notFound),
-            'message'   => $error_message,
-            'status'    => $status
-        ];
-
-        return $this->createResponse($data, $status);
+        // Else return $object->toArray()
+        return $this->createResponse($student->toArray());
     }
 
     /**
@@ -83,19 +80,42 @@ final class MasterListController extends AbstractRestController
 
     public function create(Request $request, Response $response, callable $out = null)
     {
+        $data   = $request->getParsedBody();
+
+        try {
+            $output = $this->repository->enlist($data);
+
+            $code   = 200;
+            $status = 'OK';
+            $message = sprintf("Student with id '%s' is successfully added.", $data['student_id']);
+        } catch (\Exception $ex) {
+            $code = 403;
+            $status = 'Forbidden';
+            $message = sprintf("Student with id '%s' has existing record.", $data['student_id']);
+        }
+
+        return $this->createResponse($this->formatMessage($code, $status, $message), $code);
+    }
+
+    public function update(Request $request, Response $response, callable $out = null)
+    {
+        $id = $request->getAttribute(self::IDENTIFIER_NAME);
         $data = $request->getParsedBody();
-        $output = $this->repository->enlist($data);
+        var_dump($data);
+        exit;
+        $result = $this->repository->fetchById($id);
 
-        // Throw error message if id is missing
-        $status         = 200;
-        $success        = 'OK';
-        $message        = sprintf("Student with id '%s' is successfuly added.", $data['student_id']);
-        $data = [
-            'success'   => $this->responsePhraseToCode($success),
-            'message'   => $message,
-            'status'    => $status
-        ];
+        if ($result->count() > 0) {
+            $hydrator   = $result->getHydrator();
+            $object     = $$result->getObjectPrototype();
+            $hydrator->hydrate($data, $object);
+            var_dump($object);
+            exit;
+            $result = $this->repository->update($entity->current());
+            var_dump($request);
+            exit;
+        }
 
-        return $this->createResponse($data, $status);
+        return $this->createResponse($this->formatMessage($code, $status, $message), $code);
     }
 }
